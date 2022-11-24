2eabb7b9287e0ba90fd110567edc81444b0ff256
joseviniciusb <josevbluz@outlook.com>
2022-11-24 11:08:42 -0300

    estabelecer conexao do front com back e instalar npm CORS

--- server/app.js
+++ server/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+var cors = require('cors');
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.get("/client/", (req, res) => {
+  res.status(200).json("ok");
+  res.send("Hello World!!!");
+});
+
+app.post('/', (req, res)  => {
+   
+    res.send("<h1>LALALAND from POST</h1>")
+})
+
+app.put('/', (req, res)  => {
+   
+  res.send("<h1>LALALAND from POST</h1>")
+})
+
+app.delete('/', (req, res)  => {
+   
+  res.send("<h1>LALALAND from POST</h1>")
+})
