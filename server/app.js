d9712be8a0e6dbd5450cca9bb35b816e3530b1d5
joseviniciusb <josevbluz@outlook.com>
2022-11-24 11:13:13 -0300

    fX: consertar o metodo GET

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-var cors = require('cors');
+var cors = require("cors");
 
 app.use(cors());
 
@@ -9,22 +9,18 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-app.get("/client/", (req, res) => {
-  res.status(200).json("ok");
-  res.send("Hello World!!!");
+app.get("/", (req, res) => {
+  res.send("<h1>LALALAND from GET</h1>");
 });
 
-app.post('/', (req, res)  => {
-   
-    res.send("<h1>LALALAND from POST</h1>")
-})
+app.post("/", (req, res) => {
+  res.send("<h1>LALALAND from POST</h1>");
+});
 
-app.put('/', (req, res)  => {
-   
-  res.send("<h1>LALALAND from POST</h1>")
-})
+app.put("/", (req, res) => {
+  res.send("<h1>LALALAND from PUT</h1>");
+});
 
-app.delete('/', (req, res)  => {
-   
-  res.send("<h1>LALALAND from POST</h1>")
-})
+app.delete("/", (req, res) => {
+  res.send("<h1>LALALAND from DELETE</h1>");
+});
